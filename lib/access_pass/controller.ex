b1bed03ef9c48375cbebee74a52496f99f8202ce
jordan piepkow <jpiepkow@gmail.com>
2018-08-09 18:17:11 -0400

    switched /check to return 401 status

--- lib/access_pass/controller.ex
+++ lib/access_pass/controller.ex
@@ -10,7 +10,7 @@ defmodule AccessPass.Controller do
   def check(%{method: "GET"} = conn, _) do
     case AccessPass.logged?(getHeaderValue(conn, "access_token")) do
       {:ok, meta} -> conn |> json(200, %{ok: meta})
-      {:error, errorMessage} -> conn |> json(400, %{error: errorMessage})
+      {:error, errorMessage} -> conn |> json(401, %{error: "unauthorized"})
     end
   end
 
