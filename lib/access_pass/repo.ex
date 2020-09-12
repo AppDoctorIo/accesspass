4ef0ffeade8fa3d907374a18715a0950f83fb55f
jordan piepkow <jordanpiepkow@jordans-MacBook-Pro.local>
2020-09-12 16:31:35 -0400

    changed how repo is pulled in

--- lib/access_pass/repo.ex
+++ lib/access_pass/repo.ex
@@ -1,7 +1,7 @@
 defmodule AccessPass.Repo do
   @moduledoc false
-  IO.inspect(Application.get_env(:access_pass, :repo))
-  if Application.get_env(:access_pass, :repo) == nil do
+  import AccessPass.Config
+  if repo() == nil do
     use Ecto.Repo, otp_app: :access_pass
   end
 end
